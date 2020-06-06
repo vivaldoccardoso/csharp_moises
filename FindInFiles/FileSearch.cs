52162a3f74cc531c65586d94e8dad4cce5fcd721
moisesbueno <moibueno2011@hotmail.com>
2020-06-05 21:09:41 -0300

    First commit

---
+++ FindInFiles/FileSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FindInFiles
+{
+    public static class FileSearch
+    {
+        public static string[] Find(string directory, string extension, string searchPhrase)
+        {
+            var files = Directory.GetFiles(directory, $"*.{extension}", SearchOption.AllDirectories);
+
+            return files.Where(f => f.Contains(searchPhrase)).ToArray();
+        }
+
+    }
+}
+
---
+++ FindInFiles/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindInFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var files = FileSearch.Find(@"C:\Users\moibu\Downloads", "pdf", "97");
+
+            foreach (var item in files)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
